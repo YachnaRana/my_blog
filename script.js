cf1bfe5c6bfc4c3988b1e2f5f2007bf7a1854037
YachnaRana <57913984+YachnaRana@users.noreply.github.com>
2020-06-09 19:22:02 -0400

    gif added in background and some animations

--- script.js
+++ script.js
@@ -47,55 +47,73 @@ $(document).ready(function() {
 
   /*animation on scroll*/
   $('.js--wp-1').waypoint(function (direction) {
-    $('.js--wp-1').addClass('animated rubberBand');
+    $('.js--wp-1').addClass('animated bounceInRight');
   }, {
     offset: '50%'
   });
 
   $('.js--wp-2').waypoint(function (direction) {
-    $('.js--wp-2').addClass('animated pulse');
+    $('.js--wp-2').addClass('animated zoomInRight');
   }, {
     offset: '50%'
   });
 
   $('.js--wp-3').waypoint(function (direction) {
-    $('.js--wp-3').addClass('animated flipInY');
+    $('.js--wp-3').addClass('animated pulse');
   }, {
     offset: '50%'
   });
 
   $('.js--wp-4').waypoint(function (direction) {
-    $('.js--wp-4').addClass('animated bounce');
+    $('.js--wp-4').addClass('animated rubberBand');
   }, {
     offset: '50%'
   });
 
   $('.js--wp-5').waypoint(function (direction) {
-    $('.js--wp-5').addClass('animated bounceInDown');
+    $('.js--wp-5').addClass('animated fadeInLeft');
   }, {
     offset: '50%'
   });
 
   $('.js--wp-6').waypoint(function (direction) {
-    $('.js--wp-6').addClass('animated bounceInRight');
+    $('.js--wp-6').addClass('animated fadeInRight');
   }, {
     offset: '50%'
   });
 
   $('.js--wp-7').waypoint(function (direction) {
-    $('.js--wp-7').addClass('animated bounceInLeft');
+    $('.js--wp-7').addClass('animated bounce');
   }, {
     offset: '50%'
   });
 
   $('.js--wp-8').waypoint(function (direction) {
-    $('.js--wp-8').addClass('animated bounceInUp');
+    $('.js--wp-8').addClass('animated fadeInDown');
   }, {
     offset: '50%'
   });
 
-  $('.js--iamhungry').waypoint(function () {
-    $('.js--iamhungry').addClass('animated rubberBand');
+  $('.js--wp-9').waypoint(function () {
+    $('.js--wp-9').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-10').waypoint(function () {
+    $('.js--wp-10').addClass('animated bounceInLeft');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-11').waypoint(function () {
+    $('.js--wp-11').addClass('animated zoomInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-12').waypoint(function () {
+    $('.js--wp-12').addClass('animated zoomInDown');
   }, {
     offset: '50%'
   });
